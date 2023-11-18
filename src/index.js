f9f6920da7de78cf121439744e0997ad7ff03c6f
catalindoja <37113386+catalindoja@users.noreply.github.com>
2023-11-18 13:10:59 +0100

    new changes to try and remove strict mode

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import { AuthProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
